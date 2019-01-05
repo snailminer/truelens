943d6597b6a65ca0562d93919ac2e24c3bf63455
synote <synote@qq.com>
2019-01-05 22:45:19 +0800

    add index page and main template

--- truelens/lens/urls.py
+++ truelens/lens/urls.py
@@ -16,5 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from lens.views import index
+
 urlpatterns = [
+    path(r'', index.index, name='index'),
 ]
--- truelens/lens/views/index.py
+++ truelens/lens/views/index.py
@@ -12,3 +12,8 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')

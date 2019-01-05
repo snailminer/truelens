e328ce05a26b80fd2dc13b845dacc9a07a3ca4b4
synote <synote@qq.com>
2019-01-06 01:02:31 +0800

    add static_root variable in settings

--- truelens/lens/settings.py
+++ truelens/lens/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f&p)ox1j=8njb5*^h^94k8ytof!2#odk^z!!@3(&y$-&4wsv0u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')

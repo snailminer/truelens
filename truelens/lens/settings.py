342eaf3fbd2ca5f788af79327abcd102917c6442
synote <synote@qq.com>
2019-01-06 13:12:31 +0800

    change DEBUG mode to false

--- truelens/lens/settings.py
+++ truelens/lens/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f&p)ox1j=8njb5*^h^94k8ytof!2#odk^z!!@3(&y$-&4wsv0u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
